06d4e2a26fc04148b9098bcf55c4a93ff9f3cba5
maiaarrais <maiacarrais@gmail.com>
2023-09-21 13:33:11 -0400

    Began working with variables

---
+++ experiments/Variables experiment /js/script.js
@@ -0,0 +1,40 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() 
+{
+    createCanvas(640, 480);
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    
+    background(0, 255, 100);
+    noStroke();
+    rectMode(CENTER);
+    rect(mouseX, mouseY, 100, 100); 
+
+    
+}
