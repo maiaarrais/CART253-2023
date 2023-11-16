183f7709e6e67ab9aecfed614d1fdc80cfbd922c
maiaarrais <maiacarrais@gmail.com>
2023-11-16 13:56:06 -0500

    defining circle class, x and y parameters to set the initial position

--- activities/Make some noise/js/welcomescreen.js
+++ activities/Make some noise/js/welcomescreen.js
@@ -16,3 +16,14 @@ function draw() {
 function mousePressed() {
   circles.push(new Circle(mouseX, mouseY));
 }
+
+class Circle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.radius = 20;
+    this.oscillator = new p5.Oscillator();
+    this.oscillator.setType('sine');
+    this.oscillator.start();
+  }
+}
