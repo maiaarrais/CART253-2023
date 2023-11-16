6223b7b6ce3f2fc75cd77553dadd61c65e921225
maiaarrais <maiacarrais@gmail.com>
2023-11-16 14:10:50 -0500

    adjusting variables such as amplitude (magnitude or intensity of the sound wave) and synthesizer (electronic musical instrument that generates sound)

--- activities/Make some noise/js/welcomescreen.js
+++ activities/Make some noise/js/welcomescreen.js
@@ -1,7 +1,9 @@
 let circles = [];
+let notes = ['F3', 'G3', 'Ab4', 'Bb4', 'C4', 'Db4', 'Eb4', 'F4'];
 
 function setup() {
   createCanvas(600, 400);
+  userStartAudio(); // Initialize audio
 }
 
 function draw() {
@@ -13,6 +15,7 @@ function draw() {
     circle.display();
   }
 }
+
 function mousePressed() {
   circles.push(new Circle(mouseX, mouseY));
 }
@@ -24,32 +27,39 @@ class Circle {
     this.radius = 20;
     this.oscillator = new p5.Oscillator();
     this.oscillator.setType('sine');
+    this.oscillator.amp(0.1); // Set amplitude
     this.oscillator.start();
+
+    // Synthesizer setup
+    this.note = random(notes);
+    this.synth = new p5.PolySynth();
+  }
+
+  move() {
+    this.position.add(this.velocity);
+  }
+
+  checkEdges() {
+    if (this.position.x < 0 || this.position.x > width) {
+      this.velocity.x *= -1;
+      this.playNote();
+    }
+    if (this.position.y < 0 || this.position.y > height) {
+      this.velocity.y *= -1;
+      this.playNote();
+    }
+  }
+
+  playNote() {
+    // Map the distance from the center to a musical note
+    let note = map(dist(this.position.x, this.position.y, width / 2, height / 2), 0, width / 2, 220, 880);
+    this.oscillator.freq(note);
+  }
+
+  display() {
+    fill(0);
+    ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
   }
 }
 
-move() {
-  this.position.add(this.velocity);
-}
 
-checkEdges() {
-  if (this.position.x < 0 || this.position.x > width) {
-    this.velocity.x *= -1;
-    this.playNote();
-  }
-  if (this.position.y < 0 || this.position.y > height) {
-    this.velocity.y *= -1;
-    this.playNote();
-  }
-}
-
-playNote() {
-  // Map the distance from the center to a musical note
-  let note = map(dist(this.position.x, this.position.y, width / 2, height / 2), 0, width / 2, 220, 880);
-  this.oscillator.freq(note);
-}
-
-display() {
-  fill(0);
-  ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
-}
