7f021e59d3a2cdeab8f443cd4f1b5df0afe09a43
maiaarrais <maiacarrais@gmail.com>
2023-11-16 13:59:26 -0500

    class for movement, edge checking, playing a musical note, and displaying the circle.

--- activities/Make some noise/js/welcomescreen.js
+++ activities/Make some noise/js/welcomescreen.js
@@ -27,3 +27,29 @@ class Circle {
     this.oscillator.start();
   }
 }
+
+move() {
+  this.position.add(this.velocity);
+}
+
+checkEdges() {
+  if (this.position.x < 0 || this.position.x > width) {
+    this.velocity.x *= -1;
+    this.playNote();
+  }
+  if (this.position.y < 0 || this.position.y > height) {
+    this.velocity.y *= -1;
+    this.playNote();
+  }
+}
+
+playNote() {
+  // Map the distance from the center to a musical note
+  let note = map(dist(this.position.x, this.position.y, width / 2, height / 2), 0, width / 2, 220, 880);
+  this.oscillator.freq(note);
+}
+
+display() {
+  fill(0);
+  ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
+}
