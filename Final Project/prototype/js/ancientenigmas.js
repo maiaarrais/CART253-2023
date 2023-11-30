2d488b02d7f7daa88e87f0e6402f136e3f684e7e
maiaarrais <maiacarrais@gmail.com>
2023-11-30 16:42:25 -0500

    Got the code working so that when you click a specific image, it takes you to the correct game puzzle

--- Final Project/prototype/js/ancientenigmas.js
+++ Final Project/prototype/js/ancientenigmas.js
@@ -2,12 +2,22 @@ let backgroundImage;
 let narratorImage;
 let continueButton;
 
+let isOnFirstScreen = true;
 let isOnSecondScreen = false;
+let isOnThirdScreen = false;
+
+let easyPuzzle;
+let mediumPuzzle;
+let hardPuzzle;
 
 function preload() {
   // Load your background and narrator images
   backgroundImage = loadImage('assets/images/ancientbg.jpeg');
   narratorImage = loadImage('assets/images/traveler.png');
+  //load pictures puzzle
+  easyPuzzle = loadImage('assets/images/image4.jpg');
+  mediumPuzzle = loadImage('assets/images/image6.jpg');
+  hardPuzzle = loadImage('assets/images/imageIndia.jpg');
 }
 
 function setup() {
@@ -25,13 +35,14 @@ function draw() {
     image(backgroundImage, 0, 0, width, height);
     drawNarrator();
     drawTextBox();
-  } else {
+  } else if (isOnSecondScreen){
     // Draw the second screen
-    background(200); // You can change the background color for the second screen
-    textSize(32);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    text("This is the second screen!", width / 2, height / 2);
+    background(200); 
+    drawOptions();
+  } else if (isOnThirdScreen){
+    // Draw the third screen
+    background(150); 
+    drawFinalScreen();
   }
 }
 
@@ -77,9 +88,70 @@ function drawTextBox() {
   text("Hi there! Welcome to the Ancient Civilizations.\n When you click continue you will see three puzzles,\nclick on the easy level to learn about Mesopotamia,\n on the medium level to learn about Ancient Egypt, \n and on the hard level to learn about Ancient India.", width / 2, 400);
 }
 
+
+function drawOptions() {
+  // Display three images next to each other
+  let imageSize = 350;
+  let imageSpacing = 50;
+  let imageY = height / 2 - imageSize / 2;
+
+  // Easy image
+  image(easyPuzzle, (width - 3 * imageSize - 2 * imageSpacing) / 2, imageY, imageSize, imageSize);
+
+  // Medium image
+  image(mediumPuzzle, (width - imageSize) / 2, imageY, imageSize, imageSize);
+
+  // Hard image
+  image(hardPuzzle, (width + imageSize + imageSpacing) / 2, imageY, imageSize, imageSize);
+}
+
+function drawFinalScreen() {
+  // Draw the third screen content
+  textSize(32);
+  fill(255);
+  textAlign(CENTER, CENTER);
+}
+
 function goToSecondScreen() {
+  isOnFirstScreen = false;
   isOnSecondScreen = true;
   continueButton.remove(); // Remove the continue button from the first screen
+}
+
+function mouseClicked() {
+  let imageSize = 350;
+  let imageSpacing = 50;
+  let imageY = height / 2 - imageSize / 2;
+
+  // Check if the click is within the bounds of the easy image
+  if (
+    mouseX > (width - 3 * imageSize - 2 * imageSpacing) / 2 &&
+    mouseX < (width - 3 * imageSize - 2 * imageSpacing) / 2 + imageSize &&
+    mouseY > imageY &&
+    mouseY < imageY + imageSize
+  ) {
+    window.location.href = 'easyPuzzle.html';
+  }
+
+  // Check if the click is within the bounds of the medium image
+  if (
+    mouseX > (width - imageSize) / 2 &&
+    mouseX < (width - imageSize) / 2 + imageSize &&
+    mouseY > imageY &&
+    mouseY < imageY + imageSize
+  ) {
+    window.location.href = 'mediumPuzzle.html';
+  }
+
+  // Check if the click is within the bounds of the hard image
+  if (
+    mouseX > (width + imageSize + imageSpacing) / 2 &&
+    mouseX < (width + imageSize + imageSpacing) / 2 + imageSize &&
+    mouseY > imageY &&
+    mouseY < imageY + imageSize
+  ) {
+    window.location.href = 'hardPuzzle.html';
+  }
 }
 
 
