66b0fd3be7f42b9c37b834ac68b762c140f21c20
maiaarrais <maiacarrais@gmail.com>
2023-12-06 20:48:53 -0500

    creating the last screen and updating the space one to function properly

--- Final Project/prototype/js/space.js
+++ Final Project/prototype/js/space.js
@@ -1,87 +1,91 @@
-/**
- * Dodge-em activity 
- * Maia Arrais 
- * The purpose of this program is to try and avoid all the balls that randomly appear on the screen. Good luck!
- * 
- */
-
-let balls = [];
-let user = {
-  x: 250,
-  y: 250,
-  size: 60,
-  fill: {
-    r: 0,
-    g: 204,
-    b: 204
-  }
-};
-
-let skyImg;
-
+let asteroids = [];
+let spaceship;
+let backgroundImage;
+let startTime; // Variable to store the start time
+  
 function preload() {
-  skyImg = loadImage("assets/images/sky.jpg"); //loading the image
+  spaceship = loadImage("assets/images/spaceship.png");
+
+  asteroidImage1 = loadImage("assets/images/asteroid.png");
+  asteroidImage2 = loadImage("assets/images/meteor.png");
+
+  backgroundImage = loadImage("assets/images/space.jpg");
 }
 
 function setup() {
-  createCanvas(750, 500);
-  // Image display resizing to fit canvas
-  skyImg.resize(750, 500);
+  createCanvas(windowWidth, windowHeight);
   noCursor();
   initializeProgram();
+  startTime = millis();
 }
 
 function draw() {
-  background(0);
-  image(skyImg, 0, 0);
-  noStroke();
+  
+  background(backgroundImage); // Draw the background image
+  
+  // Update and display each asteroid
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+    let asteroid = asteroids[i];
+    asteroid.move();
+    asteroid.display();
 
-  // Update and display each ball
-  for (let i = balls.length - 1; i >= 0; i--) {
-    let ball = balls[i];
-    ball.move();
-    ball.display();
+  // Check for interaction
+  let collisionRadius = (asteroid.segmentSize + spaceship.width) / 4; // Simplified collision check
+  if (dist(spaceship.x, spaceship.y, asteroid.x, asteroid.y) < collisionRadius) {
+    gameOver();
+  }
 
-    // Check for interaction
-    let d = dist(user.x, user.y, ball.x, ball.y);
-    if (d < ball.segmentSize / 2 + user.size / 2) {
-      noLoop();
-    }
-
-    // Remove balls that are out of the canvas
-    if (ball.x > width + ball.segmentSize / 2) {
-      balls.splice(i, 1);
+    // Remove asteroids that are out of the canvas
+    if (asteroid.x > width + asteroid.segmentSize / 2) {
+      asteroids.splice(i, 1);
     }
   }
 
   // User movement
-  user.x = mouseX;
-  user.y = mouseY;
+  spaceship.x = mouseX;
+  spaceship.y = mouseY;
 
-  // Create new random balls 
-  if (frameCount % 70 == 0) { // Create a new ball every 30 frames
-    let newBall = new Ball();
-    balls.push(newBall);
+  // Create new random asteroids
+  if (frameCount % 70 == 0) {
+    let newAsteroid;
+    if (random() > 0.5) {
+      newAsteroid = new Asteroid(asteroidImage1);
+    } else {
+      newAsteroid = new Asteroid(asteroidImage2);
+    }
+    asteroids.push(newAsteroid);
   }
 
-  // Display user
-  fill(user.fill.r, user.fill.g, user.fill.b);
-  ellipse(user.x, user.y, user.size);
+  // Display spaceship
+  image(spaceship, spaceship.x - spaceship.width / 2, spaceship.y - spaceship.height / 2, 60, 60);
 }
 
-class Ball {
-  constructor() {
+
+function gameOver() {
+  let playDuration = (millis() - startTime) / 1000; // Calculate play duration in seconds
+
+  if (playDuration < 45) {
+    // Restart the game
+    initializeProgram();
+  } else {
+    // Open a new window (replace 'your_url_here' with the actual URL)
+    window.open('lastScreen.html');
+  }
+
+  // Stop the game loop
+  noLoop();
+}
+
+
+class Asteroid {
+  constructor(img) {
     this.x = 0;
     this.y = random(0, height);
     this.segmentSize = 40;
     this.vx = 0;
     this.vy = 0;
     this.speed = 10;
-    this.fill = {
-      r: 51,
-      g: 51,
-      b: 255
-    };
+    this.img = img;
     this.vx = this.speed;
   }
 
@@ -91,19 +95,18 @@ class Ball {
   }
 
   display() {
-    fill(this.fill.r, this.fill.g, this.fill.b);
-    ellipse(this.x, this.y, this.segmentSize);
+    image(this.img, this.x - this.segmentSize / 2, this.y - this.segmentSize / 2, this.segmentSize, this.segmentSize);
   }
-
-  
 }
 
 function initializeProgram() {
-    balls = []; // Clear the array to start with no balls
-    loop(); // Start or resume the program loop
-  }
+  asteroids = []; // Clear the array to start with no asteroids
+  loop(); // Start or resume the program loop
+}
+
 function keyPressed() {
-    if (key === 'r' || key === 'R') {
-      initializeProgram(); // Reset the program when 'r' or 'R' is pressed
-    }
+  if (key === "r" || key === "R") {
+    initializeProgram(); // Reset the program when 'r' or 'R' is pressed
   }
+}
+
